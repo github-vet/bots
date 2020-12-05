14367484599850a513c38d08d4ef14906235b19f
K. Alex Mills <k.alex.mills@gmail.com>
2020-12-05 11:50:54 -0600

    fix: gcloud config up-front

--- docker/container-build.sh
+++ docker/container-build.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-VETBOT_IMAGE=vetbot
-TRACKBOT_IMAGE=trackbot
-
-docker build -t gcr.io/${PROJECT_PROD}/${VETBOT_IMAGE}:$TRAVIS_COMMIT -f docker/vetbot.dockerfile .
-docker build -t gcr.io/${PROJECT_PROD}/${TRACKBOT_IMAGE}:$TRAVIS_COMMIT -f docker/trackbot.dockerfile .
-
 echo $GCLOUD_SERVICE_KEY_PROD | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
@@ -15,6 +9,12 @@ gcloud --quiet config set project $PROJECT_PROD
 gcloud --quiet config set container/cluster $CLUSTER
 gcloud --quiet config set compute/zone ${ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER
+
+VETBOT_IMAGE=vetbot
+TRACKBOT_IMAGE=trackbot
+
+docker build -t gcr.io/${PROJECT_PROD}/${VETBOT_IMAGE}:$TRAVIS_COMMIT -f docker/vetbot.dockerfile .
+docker build -t gcr.io/${PROJECT_PROD}/${TRACKBOT_IMAGE}:$TRAVIS_COMMIT -f docker/trackbot.dockerfile .
 
 gcloud docker -- push gcr.io/${PROJECT_PROD}/${VETBOT_IMAGE}
 gcloud docker -- push gcr.io/${PROJECT_PROD}/${TRACKBOT_IMAGE}
